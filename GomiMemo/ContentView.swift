2ac1eefb7c22f9ea7c0410884828dda348749749
entotsu <blackn.red42@gmail.com>
2020-09-12 14:40:28 +0900

    Initial Commit

---
+++ GomiMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GomiMemo
+//
+//  Created by 岡本 拓也 on 2020/09/12.
+//  Copyright © 2020 TakuyaOkamoto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
